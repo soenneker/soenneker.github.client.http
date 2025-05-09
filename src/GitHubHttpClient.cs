ad962479d1b6f48de74853e87f71b3c644d9d627
Jake Soenneker <jake@soenneker.com>
2025-05-09 12:06:11 -0500

    delayed init

--- src/GitHubHttpClient.cs
+++ src/GitHubHttpClient.cs
@@ -4,55 +4,56 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Soenneker.Dtos.HttpClientOptions;
 using Soenneker.Extensions.Configuration;
 using Soenneker.GitHub.Client.Http.Abstract;
 using Soenneker.Utils.HttpClientCache.Abstract;
-using Soenneker.Utils.HttpClientCache.Dtos;
 
 namespace Soenneker.GitHub.Client.Http;
 
-///<inheritdoc cref="IGitHubHttpClient"/>
-public class GitHubHttpClient : IGitHubHttpClient
+/// <inheritdoc cref="IGitHubHttpClient"/>
+public sealed class GitHubHttpClient : IGitHubHttpClient
 {
     private readonly IHttpClientCache _httpClientCache;
+    private readonly IConfiguration _config;
 
-    private readonly HttpClientOptions _options;
+    private const string _clientId = nameof(GitHubHttpClient);
 
     public GitHubHttpClient(IHttpClientCache httpClientCache, IConfiguration config)
     {
         _httpClientCache = httpClientCache;
-
-        var token = config.GetValueStrict<string>("GitHub:Token");
-
-        _options = new HttpClientOptions
-        {
-            BaseAddress = "https://api.github.com/",
-            DefaultRequestHeaders = new Dictionary<string, string>
-            {
-                {"Accept", "application/vnd.github+json"},
-                {"Authorization", $"Bearer {token}"},
-                {"X-GitHub-Api-Version", "2022-11-28"},
-                {"User-Agent", Guid.NewGuid().ToString()}
-            }
-        };
+        _config = config;
     }
 
     public ValueTask<HttpClient> Get(CancellationToken cancellationToken = default)
     {
-        return _httpClientCache.Get(nameof(GitHubHttpClient), _options, cancellationToken: cancellationToken);
+        return _httpClientCache.Get(_clientId, () =>
+        {
+            var token = _config.GetValueStrict<string>("GitHub:Token");
+
+            return new HttpClientOptions
+            {
+                BaseAddress = "https://api.github.com/",
+                DefaultRequestHeaders = new Dictionary<string, string>
+                {
+                    { "Accept", "application/vnd.github+json" },
+                    { "Authorization", $"Bearer {token}" },
+                    { "X-GitHub-Api-Version", "2022-11-28" },
+                    { "User-Agent", Guid.NewGuid().ToString() }
+                }
+            };
+        }, cancellationToken);
     }
 
     public void Dispose()
     {
         GC.SuppressFinalize(this);
-
-        _httpClientCache.RemoveSync(nameof(GitHubHttpClient));
+        _httpClientCache.RemoveSync(_clientId);
     }
 
     public ValueTask DisposeAsync()
     {
         GC.SuppressFinalize(this);
-
-        return _httpClientCache.Remove(nameof(GitHubHttpClient));
+        return _httpClientCache.Remove(_clientId);
     }
 }
